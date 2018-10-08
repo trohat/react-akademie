bbea65cbc2c43e8f0178c4e4c70658abd753c24a
martin.machacek <martin.machacek@inventi.cz>
2018-10-08 17:39:55 +0200

    Add testing data, delete styles

---
+++ src/pages/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <header>
+      <h1>basic page for Wallet app</h1>
+
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
