cdca038884e77bb5116766df696d3f2ec7ab1f21
martin.machacek <martin.machacek@inventi.cz>
2018-10-10 19:11:02 +0200

    Render transactions, add styled-components

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
 
-const App = () => (
-  <div>
-    <header>
-      <h1>basic page for Wallet app</h1>
+import Transaction from "../components/Transaction";
+import data from "../data.json";
 
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const Button = styled.button`
+  color: red;
+`;
+
+class App extends Component {
+  state = {
+    transactions: []
+  };
+
+  componentDidMount() {
+    this.setState({ transactions: data });
+  }
+
+  addTransaction = () => {
+    this.setState(prevState => ({
+      transactions: [
+        ...prevState.transactions,
+        {
+          name: "Moje",
+          value: 100,
+          type: "income"
+        }
+      ]
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>basic page for Wallet app</h1>
+          <Button onClick={this.addTransaction}>Přidat</Button>
+          {this.state.transactions.map(({ name, value, type, id }) => (
+            <Transaction key={id} name={name} value={value} type={type} />
+          ))}
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
